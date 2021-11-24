bb4f6b1e7ae6fed1d1ddc328e00f5ed5e1bc74e9
phamkhoa1303 <pham.duykhoa1303@gmail.com>
2021-11-24 17:27:46 +0700

    khoa

--- lab5Main.c
+++ lab5Main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 #include <string.h>
+#include <ctype.h>
 #define SIZE 100
 
 void Continues(int *z)
@@ -26,41 +25,9 @@ void Q3()
 {
 }
 //    khoa
-void Q4()
-{
-    char x[SIZE], x1[SIZE];
-    printf("nhap chuoi can xu li:");
-    fgets(x, SIZE, stdin);
-    int dai = strlen(x);
-    printf("mang vua nhap la:%s\n", x);
-    // printf("mang sau khi xoa khoang trang:%s\n", delete_space(x, x1));
-}
-// char *delete_space(char *str, char *ketqua)
-// {
-//     int pos = 0;
-//     const int len = strlen(str);
-//     if (str[0] != ' ')
-//     {
-//         ketqua[pos++] = str[0] - 32;
-//     }
-//     for (int i = 1; i < len; i++)
-//     {
-//         char c = str[i];
-//         if (c == ' ' && str[i + 1] == ' ')
-//         {
-//             continue;
-//         }
-//         if (str[i - 1] == ' ' && c != ' ' && i < len - 1)
-//             ketqua[pos++] = c - 32;
-//         else
-//         {
-//             char a = tolower(c);
-//             ketqua[pos++] = c;
-//         }
-//     }
-//     return ketqua;
-// }
 
+char *delete_space(char *str, char *ketqua);
+void Q4();
 void Q5()
 {
 }
@@ -91,8 +58,7 @@ void Q11()
 
 int main()
 {
-    int n, m, z;
-    srand(time(NULL));
+    int z;
 
     do
     {
@@ -182,4 +148,40 @@ int main()
     } while (z != 0);
 
     return 0;
+}
+
+void Q4()
+{
+    fflush(stdin);
+    char x[SIZE], x1[SIZE];
+    printf("nhap chuoi can xu li:");
+    fgets(x, SIZE, stdin);
+    // int dai = strlen(x);
+    printf("mang vua nhap la:%s\n", x);
+    printf("mang sau khi xoa khoang trang:%s\n", delete_space(x, x1));
+}
+char *delete_space(char *str, char *ketqua)
+{
+    int pos = 0;
+    const int len = strlen(str);
+    if (str[0] != ' ')
+    {
+        ketqua[pos++] = str[0] - 32;
+    }
+    for (int i = 1; i < len; i++)
+    {
+        char c = str[i];
+        if (c == ' ' && str[i + 1] == ' ')
+        {
+            continue;
+        }
+        if (str[i - 1] == ' ' && c != ' ' && i < len - 1)
+            ketqua[pos++] = c - 32;
+        else
+        {
+            // char a = tolower(c);
+            ketqua[pos++] = c;
+        }
+    }
+    return ketqua;
 }
