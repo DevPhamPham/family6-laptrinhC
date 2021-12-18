93ed94def4053d4e0e4fc741dc92cb5f7dc38d61
phamkhoa1303 <pham.duykhoa1303@gmail.com>
2021-12-18 22:10:30 +0700

    32,42,43

--- lab1000.c
+++ lab1000.c
@@ -13,6 +13,11 @@ void bai14();
 int B14(int ,int);
 void bai15();
 int B15(int ,int);
+void bai32();
+void bai42();
+void bai43();
+int findKB42(int,int n,int,int,int);
+int demsSo43(int n,int c);
 void NhapN(int *n)
 {
     do
@@ -185,17 +190,17 @@ int main()
         printf("\n\n08: S = (2n + 1)/(2n + 2)");
         printf("\n\n09: S = n!");
         printf("\n\n10: S = x^n");
-        printf("\n\nCau 11: Tinh S(n) = 1 + 1 x 2 + 1 x 2 x 3 + ⋯ + 1 x 2 x 3 x ... x n");
-        printf("\n\nCau 12: Tinh S(n) = x + x^2 + x^3 + ⋯ + x^n");
-        printf("\n\nCau 13: Tinh S(n) = x^2 + x^4 + ⋯ + x^2n");
-        printf("\n\nCau 14: Tinh S(n) = x + x^3 + x^5 ... + x^2n+1");
-        printf("\n\nCau 15: Tinh S(n) = 1 + 1/(1+ 2)+1/(1+2+3)+ ... + 1/(1+2+...+n)");
+        printf("\n\n11: Tinh S(n) = 1 + 1 x 2 + 1 x 2 x 3 + ⋯ + 1 x 2 x 3 x ... x n");
+        printf("\n\n12: Tinh S(n) = x + x^2 + x^3 + ⋯ + x^n");
+        printf("\n\n13: Tinh S(n) = x^2 + x^4 + ⋯ + x^2n");
+        printf("\n\n14: Tinh S(n) = x + x^3 + x^5 ... + x^2n+1");
+        printf("\n\n15: Tinh S(n) = 1 + 1/(1+ 2)+1/(1+2+3)+ ... + 1/(1+2+...+n)");
         printf("\n\n29: Tim uoc so le lon nhat cua so nguyen duong n.");
         printf("\n\n30: Kiem tra so nguyen duong N co phai so hoan thien hay khong.");
         printf("\n\n31: Kiem tra so nguyen duong N co phai so nguyen to hay khong.");
-        printf("\n\nCau 32: Cho so nguyen duong n. Kiem tra so nguyen duong n co phai la so chinh phuong hay khong?");
-        printf("\n\nCau 42: Cho so nguyen duong n. Kiem tra so nguyen duong n có phai la so chinh phuong hay khong?");
-        printf("\n\nCau 43: Hay Dem so luong chu so cua so nguyen duong n");
+        printf("\n\n32: Cho so nguyen duong n. Kiem tra so nguyen duong n co phai la so chinh phuong hay khong?");
+        printf("\n\n42: Cho n la so nguyen duong. Hay tim gia tri nguyen duong k lon nhat sao cho S(k) < n. Trong do chuoi S duoc dinh nghia nhu sau: S(k)=1+2+3+...+k.");
+        printf("\n\n43: Hay Dem so luong chu so cua so nguyen duong n");
         printf("\n\nNhap lua chon cua ban: ");
         scanf("%d", &choice);
 
@@ -236,13 +241,13 @@ int main()
         bai15();
         break;
     case 32:
-        
+        bai32();
         break;
     case 42:
-        
+        bai42();
         break;
     case 43:
-        
+        bai43();
         break;
 
     case 6:
@@ -355,4 +360,40 @@ void bai15(){
 int B15(int n,int x){
     if (n < 0) return 0;
     return pow(x,2*n+1)+B15(n-1,x);
+}
+void bai32(){
+    int n,i;
+    printf("\nNhap n: ");
+    scanf("%d", &n);
+    if (sqrt(n)*sqrt(n)==n){
+        printf("\nBai 32: Day la so chinh phuong");
+    } else{
+        printf("\nDay khong phai la so chinh phuong");
+    }
+}
+void bai42(){
+    int n;
+    printf("\nNhap n: ");
+    scanf("%d", &n);
+    printf("\n\tBai 42: k = %d",findKB42(n,n,0,1,n));
+}
+int findKB42(int nA,int n,int sum,int i,int nho){
+    // printf("\nNA,N,SUM = %d,%d,%d",n,nA,sum);
+    if (n==0){
+    if (sum>=nA) return findKB42(nA,nA-i,0,i+1,nA-i);
+    if (sum<nA) return nho;
+    }
+    sum+=n;
+    return findKB42(nA,n-1,sum,i,nho);
+}
+void bai43(){
+    int n;
+    printf("\nNhap n: ");
+    scanf("%d", &n);
+    printf("\nBai 43: %d co %d chu so.",n,demsSo43(n,1));
+}
+int demsSo43(int n,int c){
+    if (n<10) return c;
+    c++;
+    return demsSo43(n/10,c);
 }
