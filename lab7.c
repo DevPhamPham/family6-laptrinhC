7979fd58fcd441ec0ad77542587e23f52ff52348
Phu Quõc <you@example.com>
2021-12-19 21:39:06 +0700

    selection sort

--- lab7.c
+++ lab7.c
@@ -1,19 +1,84 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+#define MAX 30
 
+void XuatMang(int a[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\t", a[i]);
+    }
+    
+}
 
+void NhapMang(int a[], int *n)
+{
+    int min, max;
+    do
+    {
+        printf("\n\nNhap so phan tu cua mang: ");
+        scanf("%d", &*n);
+        if (*n < 1 || *n > MAX)
+        {
+            printf("\n\nNhap sai. Vui long nhap lai.");
+        }
+        
+    } while (*n < 1 || *n > MAX);
 
+    printf("\n\nNhap khoang random: ");
+    printf("\n\nMin: ");
+    scanf("%d", &min);
+    printf("\n\nMax: ");
+    scanf("%d", &max);
+
+    for (int i = 0; i < *n; i++)
+    {
+        a[i] = rand() % (max - min + 1) + min ;
+    }
+    printf("\n\nMang da nhap:\n");
+    XuatMang(a, *n);
+}
+
+void SelectionSort(int a[], int n)
+{
+    int temp, max, start;
+    for (int start = 0; start < n - 1; start++)
+    {
+        max = start;
+        for (int i = start + 1; i < n; i++)
+        {
+            if (a[max] < a[i])
+            {
+                max = i;
+            }
+        }
+        temp = a[start];
+        a[start] = a[max];
+        a[max] = temp;
+    }
+    printf("\n\nMang sau khi sap xep:\n");
+    XuatMang(a, n);
+}
+
+void Q3()
+{
+    int a[MAX], n;
+    NhapMang(a, &n);
+    SelectionSort(a, n);
+}
 
 int main()
 {
-    int choice,n;
+    srand(time(NULL));
+    int choice;
     do
     {
-        printf("\n\nCau 1: Tinh giai thua n");
-        printf("\n\nCau 2: Tinh tong le");
-        printf("\n\nCau 3: Tinh tich le");
-        printf("\n\nCau 4: Tinh tong binh phuong");
-        printf("\n\nCau 5: Kiem tra n co toan le");
+        printf("\n\nCau 1: ");
+        printf("\n\nCau 2: ");
+        printf("\n\nCau 3: Implement the selection sort algorithm for sorting a list in descendent list. ");
+        printf("\n\nCau 4: ");
         printf("\n\nNhap lua chon cua ban: ");
         scanf("%d", &choice);
 
@@ -28,18 +93,12 @@ int main()
         break;
 
     case 3:
-        
+        Q3();
         break;
 
     case 4:
         
         break;
-
-    case 5:
-        
-        break;
-
-    
 
     default:
         printf("\n\nKhong co lua chon nay. Vui long nhap lai");
